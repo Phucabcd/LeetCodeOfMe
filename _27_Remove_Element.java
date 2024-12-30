26aeb299502305f06f08ae7a933b817745b4bd72
TrongPhuc <nguyentrongphuc03@gmail.com>
2024-12-30 17:08:30 +0700

    27 remove elenment

---
+++ _27_Remove_Element.java
@@ -0,0 +1,21 @@
+public class _27_Remove_Element {
+    public int removeElement(int[] nums, int val) {
+        int n = nums.length;
+
+        for (int i = 0; i < n;) {
+            if (nums[i] == val) {
+                for (int j = 0; j <= n - 2; j++) {
+                    nums[j] = nums[j + 1];
+                }
+                n--;
+            }else{
+                i++;
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
