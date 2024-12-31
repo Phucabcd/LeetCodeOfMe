3920f10dcfa32517146c8927802b40b7b2b49c5e
TrongPhuc <nguyentrongphuc03@gmail.com>
2024-12-31 20:14:29 +0700

    update skil 22,88

--- _27_Remove_Element.java
+++ _27_Remove_Element.java
@@ -1,19 +1,34 @@
 public class _27_Remove_Element {
-    public int removeElement(int[] nums, int val) {
-        int n = nums.length;
+    //2 vong lap for 
+    // public int removeElement(int[] nums, int val) {
+    //     int n = nums.length;
 
-        for (int i = 0; i < n;) {
-            if (nums[i] == val) {
-                for (int j = 0; j <= n - 2; j++) {
-                    nums[j] = nums[j + 1];
-                }
-                n--;
-            }else{
-                i++;
+    //     for (int i = 0; i < n;) {
+    //         if (nums[i] == val) {
+    //             for (int j = 0; j <= n - 2; j++) {
+    //                 nums[j] = nums[j + 1];
+    //             }
+    //             n--;
+    //         }else{
+    //             i++;
+    //         }
+    //     }
+    //     return 0;
+    // }
+
+    //2 con tro
+    public int removeElement2(int[] nums, int val) {
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {   
+            if (nums[i] != val) {
+                nums[k] = nums[i]; 
+                k++;
             }
         }
-        return 0;
+        return k;
     }
+
+
 
     public static void main(String[] args) {
 
--- _88_Merge_Sorted_Array.java
+++ _88_Merge_Sorted_Array.java
@@ -1,36 +1,60 @@
 public class _88_Merge_Sorted_Array {
-    public static void merge(int[] n1, int m, int[] n2, int n) {
-        for (int ai : n2) {
-            chenPhanTuVaoMang(ai, n1, m);
-            m++;
-        }
-    }
 
-    private static void chenPhanTuVaoMang(int x, int[] n1, int m) {
-        boolean timK = false; 
-        for (int i = 0; i < m; i++) {
-            if(n1[i] > x){
-                timK = true;
-                
-                for (int j = m-1; j >= i; j--) {
-                     n1[j+1] = n1[i];
-                }
-                n1[i] = x;
-                break;
+    // public static void merge(int[] n1, int m, int[] n2, int n) {
+    // for (int ai : n2) {
+    // chenPhanTuVaoMang(ai, n1, m);
+    // m++;
+    // }
+    // }
+
+    // private static void chenPhanTuVaoMang(int x, int[] n1, int m) {
+    // boolean timK = false;
+    // for (int i = 0; i < m; i++) {
+    // if(n1[i] > x){
+    // timK = true;
+
+    // for (int j = m-1; j >= i; j--) {
+    // n1[j+1] = n1[i];
+    // }
+    // n1[i] = x;
+    // break;
+    // }
+    // }
+
+    // if (timK == false) {
+    // n1[m] = x;
+    // }
+
+    // }
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = (m + n) - 1;
+
+        while (k >= 0) {
+            if (j < 0) {
+                nums1[k] = nums1[i];
+                i--;
+            } else if (i < 0) {
+                nums2[k] = nums2[j];
+                j--;
+            } else if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums2[k] = nums2[j];
+                j--;
             }
-        }    
-
-        if (timK == false) {
-            n1[m] = x;
         }
-        
+        k--;
     }
 
     public static void main(String[] args) {
-        int[] n1 = {0,1,3,0,0,0};
-        int[] n2 = {4,6,3};
+        // int[] n1 = {0,1,3,0,0,0};
+        // int[] n2 = {4,6,3};
 
-        merge(n1, 0, n2, 0);
-        System.out.println("Done: " );
+        // merge(n1, 0, n2, 0);
+        // System.out.println("Done: " );
     }
 }
