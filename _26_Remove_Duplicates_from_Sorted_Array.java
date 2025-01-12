9eea8d478da9bf5ee72c7d7733ef2c9b6be8141d
TrongPhuc <nguyentrongphuc03@gmail.com>
2025-01-12 16:58:55 +0700

    remove duplicates

---
+++ _26_Remove_Duplicates_from_Sorted_Array.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class _26_Remove_Duplicates_from_Sorted_Array {
+    
+    public int removeDuplicates(int[] nums) {
+        int n = nums.length;
+
+        if(n == 0){
+            return n;
+        }
+
+        int i = 1; int j = 1;
+
+        while (j < n) {
+            if (nums[j] != nums[j - 1]) {
+                nums[i] = nums[j];
+                i++;
+            }
+            j++;
+        }
+
+        return i;
+    }
+
+}
