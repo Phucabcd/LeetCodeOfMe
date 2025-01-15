330d382be0750be83d870a28bb73b0c4268f6b40
TrongPhuc <nguyentrongphuc03@gmail.com>
2025-01-15 13:24:00 +0700

    Reverse String

---
+++ _344_Reverse_String.java
@@ -0,0 +1,16 @@
+public class _344_Reverse_String {
+    public void reverseString(char[] s) {
+        int n = s.length;
+        int i = 0;
+        int j = n - 1;
+
+        char temp;
+        while (i < j) {
+            temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
