0c56b4285e1c68377b7d8df193fd69de4184aec9
TrongPhuc <nguyentrongphuc03@gmail.com>
2025-03-24 20:02:18 +0700

    58 length of last work

--- _35_Search_Insert_Position.java
+++ _35_Search_Insert_Position.java
@@ -1,9 +1,11 @@
-public int searchInsert(int[] nums, int target) {
+public class _35_Search_Insert_Position {
+    public int searchInsert(int[] nums, int target) {
         
-    for(int i = 0; i < nums.length; i++){
-        if(target <= nums[i]){
-            return i;
+        for(int i = 0; i < nums.length; i++){
+            if(target <= nums[i]){
+                return i;
+            }
         }
+        return nums.length;
     }
-    return nums.length;
 }
---
+++ _58_Length_of_Last_Word.java
@@ -0,0 +1,13 @@
+public class _58_Length_of_Last_Word {
+    public int lengthOfLastWord(String s) {
+        int length = 0;
+        for(int i = s.length() - 1; i >= 0; i--){
+            if(s.charAt(i) != ' '){
+                length++;
+            }else{
+                 if(length > 0) return length;
+            }
+        }
+        return length;
+    }
+}
