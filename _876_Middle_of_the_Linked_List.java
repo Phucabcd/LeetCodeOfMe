c90d4a6f22fea648883a5ce3498631d26cf44778
TrongPhuc <nguyentrongphuc03@gmail.com>
2025-04-01 22:46:48 +0700

    fast and slow pointer

---
+++ _876_Middle_of_the_Linked_List.java
@@ -0,0 +1,12 @@
+public class _876_Middle_of_the_Linked_List {
+    // public ListNode middleNode(ListNode head) {
+    //     ListNode slow = head, fast = head;
+
+    //     while(fast != null && fast.next != null){
+    //         slow = slow.next;
+    //         fast = fast.next.next;
+    //     }
+        
+    //     return slow;
+    // }
+}
