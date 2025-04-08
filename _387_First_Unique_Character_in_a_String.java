fafffbe75755c51566345fb3cb90236444478485
TrongPhuc <nguyentrongphuc03@gmail.com>
2025-04-08 21:29:48 +0700

    add solution 387

--- _387_First_Unique_Character_in_a_String.java
+++ _387_First_Unique_Character_in_a_String.java
@@ -1,16 +1,46 @@
+import java.util.HashMap;
 
 public class _387_First_Unique_Character_in_a_String {
     public int firstUniqChar(String s) {
+      // solution 1
+      // for (int i = 0; i < s.length(); i++) {
+      //   char currentChar = s.charAt(i);
+      //   if (s.indexOf(currentChar) == s.lastIndexOf(currentChar)) {
+      //       return i; 
+      //   }
+      // }
+      //  return -1;
+
+      //solution 2 for
+      // for (int i = 0; i < s.length(); i++) {
+      //   char currentChar = s.charAt(i);
+      //   boolean isUnique = true;
+
+      //   for (int j = 0; j < s.length(); j++) {
+      //       if (i != j && currentChar == s.charAt(j)) {
+      //           isUnique = false;
+      //           break;
+      //       }
+      //   }
+
+      //   if (isUnique) {
+      //       return i;
+      //   }
+      // }
+      // return -1;
+
+      //solution 3 HashMap
+      HashMap<Character, Integer> map = new HashMap<>();
+      for(char c : s.toCharArray()){
+        map.put(c, map.getOrDefault(c, 0) + 1);
+      }
       for (int i = 0; i < s.length(); i++) {
-        char currentChar = s.charAt(i);
-        if (s.indexOf(currentChar) == s.lastIndexOf(currentChar)) {
-            return i; 
+        if (map.get(s.charAt(i)) == 1) {
+          return i;
         }
       }
-       return -1;
+      return -1;
     }
 
-    public static void main(String[] args) {
-        
-    }
+   
 }
