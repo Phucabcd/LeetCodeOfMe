f909e55ab1efdd7cb8dfab48ba6743375728ee03
TrongPhuc <nguyentrongphuc03@gmail.com>
2025-04-09 11:18:18 +0700

    add time complexity 232

--- _232_Implement_Queue_using_Stacks.java
+++ _232_Implement_Queue_using_Stacks.java
@@ -4,15 +4,19 @@ public class _232_Implement_Queue_using_Stacks {
     Stack<Integer> stack1;
     Stack<Integer> stack2;
 
+    // time complexity 
+   
     public MyQueue() {
         stack1 = new Stack<>();
         stack2 = new Stack<>();
     }
     
+    // O(1)
     public void push(int x) {
         stack1.push(x);
     }
     
+    // O(n)
     public int pop() {
         if(stack2.isEmpty()){
            while(!stack1.isEmpty()){
@@ -22,6 +26,7 @@ public class _232_Implement_Queue_using_Stacks {
         return stack2.pop();
     }
     
+    // O(n)
     public int peek() {
         if(stack2.isEmpty()){
             while(!stack1.isEmpty()){
@@ -31,6 +36,7 @@ public class _232_Implement_Queue_using_Stacks {
         return stack2.peek();
     }
     
+    // O(1)
     public boolean empty() {
        return stack1.isEmpty() && stack2.isEmpty();
     }
