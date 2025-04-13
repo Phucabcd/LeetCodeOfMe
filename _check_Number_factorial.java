a4872b771e77a83fb858ac57f690143163b2e698
TrongPhuc <nguyentrongphuc03@gmail.com>
2025-04-13 21:30:06 +0700

    check number factorial

---
+++ _check_Number_factorial.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class _check_Number_factorial {
+    // Người Ai Cập không hề thích con số 0, 
+    // vì vậy khi tính giá trị giai thừa của một số họ sẽ tìm cách bỏ các con số 0 
+    // ở cuối cùng đi để nhìn cho đẹp. Bạn hãy viết chương trình tìm chữ số cuối cùng khác 0 
+    // của n! với n là số nguyên dương được nhập vào từ bàn phím
+    public static int factorial(int n) {
+        int result = 1;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+  
+  public static int check(int n){
+      int fact = factorial(n);
+      
+      while(fact % 10 == 0){
+        fact /= 10;
+      }
+      
+      return fact % 10;
+    }
+  
+  public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(check(n));  
+    }
+
+    
+}
