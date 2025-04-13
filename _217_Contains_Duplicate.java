06c5d6a0b85f3aa9d0cf1555bef7790b330c6d91
TrongPhuc <nguyentrongphuc03@gmail.com>
2025-04-13 21:02:52 +0700

    used contains Set

---
+++ _217_Contains_Duplicate.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+import java.util.Set;
+
+// public class _217_Contains_Duplicate {
+//     Set<Integer> set = new HashSet<>();
+//         for(int i = 0; i < nums.length; i++){
+//            if (set.contains(nums[i])){
+//             return true;
+//            }
+
+//            set.add(nums[i]);
+//         }
+//         return false;
+// }
