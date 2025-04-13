9bc0422b6dfa60c8a4ffcfed7c13ebd8c1c7f320
TrongPhuc <nguyentrongphuc03@gmail.com>
2025-04-13 22:35:58 +0700

    recursive 2

--- _recursive.java
+++ _recursive.java
@@ -1,16 +1,26 @@
 public class _recursive {
     
-    public static void recurse(int n){
-        if(n > 4){
-            return;
-        }
+    // public static void recurse(int n){
+    //     if(n > 4){
+    //         return;
+    //     }
 
-        System.out.println("n : " + n);
-        recurse(n + 1);
-        System.out.println("back n : " + n);
-    }
+    //     System.out.println("n : " + n);
+    //     recurse(n + 1);
+    //     System.out.println("back n : " + n);
+    // }
+
+    public static int result(int n){
+        if(n <= 1){
+          return 1;
+        }
+        return n * result(n - 1);
+        
+        
+      }
 
     public static void main(String[] args) {
-        recurse(0);
+        // recurse(1);
+        System.out.println(result(4));
     }
 }
