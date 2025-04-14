441b581b29e65a5cdf183b54bc9d93b3a5ddff94
TrongPhuc <nguyentrongphuc03@gmail.com>
2025-04-14 15:45:12 +0700

    binary search first easy

---
+++ _704_Binary_Search.java
@@ -0,0 +1,24 @@
+public class _704_Binary_Search {
+    
+    public static int search(int[] nums, int target) {
+            int  l = 0, r = nums.length - 1;
+            while (l <= r) {
+                int mid = l + (r - l) / 2;
+                if (nums[mid] == target) {
+                    return mid;
+                } else if (nums[mid] < target) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        return -1;    
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,5,7,8};
+        int target = 7;
+        System.out.println(search(nums, target));
+    }
+
+}
