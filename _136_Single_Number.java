1ec25d314f00287ae8abfa2ee1c4773aad88363e
TrongPhuc <nguyentrongphuc03@gmail.com>
2025-04-24 21:49:22 +0700

    XOR bit-wise

---
+++ _136_Single_Number.java
@@ -0,0 +1,9 @@
+public class _136_Single_Number {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for(int num : nums){
+         result ^= num;
+        }
+         return result;
+     }
+}
