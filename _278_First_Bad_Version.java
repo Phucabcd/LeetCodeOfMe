287263ff6a26174f1f194a4c63e81a4be416574d
TrongPhuc <nguyentrongphuc03@gmail.com>
2025-04-25 10:36:12 +0700

    278 binary search

---
+++ _278_First_Bad_Version.java
@@ -0,0 +1,14 @@
+public class _278_First_Bad_Version {
+    // public int firstBadVersion(int n) {
+    //     int l = 1, r = n - 1;
+    //     while(l < r){
+    //         int mid = l + (r - l) / 2;
+    //         if(isBadVersion(mid)){
+    //             r = mid;
+    //         }else{
+    //             l = mid + 1;
+    //         }
+    //     }
+    //     return l;
+    // }
+}
