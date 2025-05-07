0cbf51aa9d66ceaa7e94a02c0df0a5e8c473f6d3
TrongPhuc <nguyentrongphuc03@gmail.com>
2025-05-07 21:22:55 +0700

    hashMap solution 1

---
+++ _49_Group_Anagrams.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class _49_Group_Anagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        //solution 1 : hashMap
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for(String str : strs){
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            if(!map.containsKey(key)) map.put(key, new ArrayList<>());
+            map.get(key).add(str);
+
+        //    String sort = Arrays.sort(str);
+        //    String key = sort.add(map.key());
+        //    if(!key.isEmpty()) str.add(map.value());
+        }
+
+        return new ArrayList<>(map.values());
+
+        //solution 2 : array 26 types
+    }
+}
