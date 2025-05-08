8afcab0e71a1ddc9fffd315c163f2284a2f8bf29
TrongPhuc <nguyentrongphuc03@gmail.com>
2025-05-08 20:37:10 +0700

    update solution 2

--- _49_Group_Anagrams.java
+++ _49_Group_Anagrams.java
@@ -7,22 +7,38 @@ public class _49_Group_Anagrams {
     public List<List<String>> groupAnagrams(String[] strs) {
 
         //solution 1 : hashMap
+        // HashMap<String, List<String>> map = new HashMap<>();
+
+        // for(String str : strs){
+        //     char[] chars = str.toCharArray();
+        //     Arrays.sort(chars);
+            
+        //     String key = new String(chars);
+        //     if(!map.containsKey(key)) map.put(key, new ArrayList<>());
+        //     map.get(key).add(str);
+
+        // //    String sort = Arrays.sort(str);
+        // //    String key = sort.add(map.key());
+        // //    if(!key.isEmpty()) str.add(map.value());
+        // }
+
+        // return new ArrayList<>(map.values());
+
+        //solution 2 : array 26 types
+
         HashMap<String, List<String>> map = new HashMap<>();
 
-        for(String str : strs){
-            char[] chars = str.toCharArray();
-            Arrays.sort(chars);
-            String key = new String(chars);
+        for (String str : strs) {
+            int[] count = new int[26];
+            for (char c : str.toCharArray()) {
+                count[c - 'a']++;
+            }
+            String key = Arrays.toString(count);
             if(!map.containsKey(key)) map.put(key, new ArrayList<>());
             map.get(key).add(str);
-
-        //    String sort = Arrays.sort(str);
-        //    String key = sort.add(map.key());
-        //    if(!key.isEmpty()) str.add(map.value());
         }
 
         return new ArrayList<>(map.values());
-
-        //solution 2 : array 26 types
+        
     }
 }
